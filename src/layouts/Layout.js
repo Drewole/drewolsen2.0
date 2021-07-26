54285d997edf1289f55808cd0262579f3dc86bef
Drew Olsen <olsenj.andrew@gmail.com>
2021-07-26 16:08:04 -0500

    Changed image to flickr account

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -14,7 +14,7 @@ const Layout = ({ children }) => (
       <meta name="description" content="Drew Olsen's Personal Portfolio" />
       <meta name="title" property="og:title" content="Drew Olsen's Portfolio" />
       <meta property="og:type" content="Website" />
-      <meta name="image" property="og:image" content="https://drewolsen.design/arrowScreenie.jpg" />
+      <meta name="image" property="og:image" content="https://flic.kr/p/2mdvcNw" />
       <meta name="description" property="og:description" content="Drew Olsen's Portfolio" /><meta name="author" content="Drew Olsen" />
       <meta name={siteMetadata.author} description={siteMetadata.description} />
       <title>{siteMetadata.title}</title>
